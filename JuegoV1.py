a9f4bf4ad6399f0fa73b97dd40199bbe535ca636
benjavilla008-lgtm <benjavilla008@gmail.com>
2026-06-21 20:12:26 -0300

    Fix print messages for consistency and style

--- JuegoV1.py
+++ JuegoV1.py
@@ -1,7 +1,7 @@
 import random
 
 def juego_de_sumas():
-    print(" ¡Bienvenido al Juego de sumar! ")
+    print(" bienvenido al Juego de suma ")
     preguntas_totales = 5
     puntaje = 0
     
@@ -12,18 +12,18 @@ def juego_de_sumas():
         
         print(f"Pregunta {i}: ¿Cuánto es {num1} + {num2}?")
         try:
-            respuesta_usuario = int(input("Tu respuesta: "))
+            respuesta_usuario = int(input("respuesta: "))
         except ValueError:
-            print("Por favor, ingresa solo números.")
+            print("por favor, solo números.")
             respuesta_usuario = None
 
         if respuesta_usuario == resultado_correcto:
-            print("¡Correcto! ¡Buen trabajo! 🎉\n")
+            print("correcto 🎉\n")
             puntaje += 1
         else:
-            print(f"Incorrecto. El resultado correcto era {resultado_correcto}. 💡\n")
+            print(f"incorrecto. el resultado correcto era {resultado_correcto}. \n")
             
-    print("--- Juego Terminado ---")
-    print(f"Tu puntaje final es: {puntaje} de {preguntas_totales}")
+    print("--- el juego Terminado ---")
+    print(f"tu puntaje final es: {puntaje} de {preguntas_totales}")
 
 juego_de_sumas()
